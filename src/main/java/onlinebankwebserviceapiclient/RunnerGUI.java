0db61e31dc03d1f904a9a74d243ff8912a487b20
Carlos Neia <x12116394@students.ncirl.ie>
2018-08-05 20:17:42 +0100

    GUI added, but still incomplete

--- src/main/java/onlinebankwebserviceapiclient/RunnerGUI.java
+++ src/main/java/onlinebankwebserviceapiclient/RunnerGUI.java
@@ -219,7 +219,7 @@ public class RunnerGUI extends javax.swing.JFrame {
                     .addComponent(transferToTf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel6)
                     .addComponent(transferAmountTf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 210, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 223, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(24, 24, 24))
         );
